abb53451e262f9a8b3ae4a7ee2a8cdd6f9981e13
Ajitesh Kumar <ajinest6@gmail.com>
2024-05-14 14:26:18 -0400

    allow logging to be disables

--- Log/Log.go
+++ Log/Log.go
@@ -9,8 +9,19 @@ var NORMAL = "\033[0m"
 
 var REDBWHITEF = "\033[37;101m"
 
+var Logging bool = true
+
+func On() {
+	Logging = true
+}
+func Off() {
+	Logging = false
+}
+
 func Statement(query string) {
-	fmt.Printf("%sDatabase:%s %s\n", BOLD, NORMAL, query)
+	if Logging == true {
+		fmt.Printf("%sDatabase:%s %s\n", BOLD, NORMAL, query)
+	}
 }
 func Error(error string) {
 	fmt.Printf("%s%sERROR%s: %s\n", REDBWHITEF, BOLD, NORMAL, error)
