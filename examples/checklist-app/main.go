b0d345e7c3adb7bbf98b5b9da8fa48cdc052e6f8
Ajitesh Kumar <ajinest6@gmail.com>
2024-08-15 15:11:04 -0400

    migrations for null and not null

--- examples/checklist-app/main.go
+++ examples/checklist-app/main.go
@@ -8,7 +8,8 @@ import (
 )
 
 type User struct {
-	ID             int
+	PublicKeyHex   string
+	ID             int `kind:"IntegerField"`
 	Username       string
 	Email          string `validators:"email"`
 	HashedPassword string
@@ -37,20 +38,26 @@ func main() {
 	user.Save()
 
 	start := time.Now()
+	one := 1
+	two := 2
+	three := 3
 	row1 := user.NewNE(&User{
-		ID:             1,
+		PublicKeyHex:   "040cb72f6902e9f29773c594a63ad4ef690aefde9d53c0764e6c532622fd58566fe62609c699d2b58d41d0e5ca2e425577dd69b824b5e3e3891bb5caa818fd75c5",
+		ID:             one,
 		Username:       "ajiteshkumar",
 		Email:          "ajiteshkumar@smtp.local",
 		HashedPassword: "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9",
 	})
 	row2 := user.NewNE(&User{
-		ID:             2,
+		PublicKeyHex:   "0447471e608fadc63e9667d8e90b44432df6330deb168e371f7f48e599eba5c0222ce06ab5411d6383097badb006ccf5473e972c674dbd0bc66fe0ab278e4fb7cb5498af91f3664366a5f37ba56114bfe039fb1a896a1e47ab990a7f9726edf7f1",
+		ID:             two,
 		Username:       "testuser1",
 		Email:          "testuser1@smtp.local",
 		HashedPassword: "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f",
 	})
 	row3 := user.NewNE(&User{
-		ID:             3,
+		PublicKeyHex:   "049a62d65eb6111ced9af5a3ab4c82c918e1ef444b9cedbc8621fab246d8d4b7ceb19a979a70a0404101a70e508762b02c4d08ffdfc66dc057e08f46d1e2dd285f8ada6bada4630c793561b900f4f78c1e4a915ecd919c28f2765154df1b40462c",
+		ID:             three,
 		Username:       "testuser2",
 		Email:          "helloworld@smtp.local",
 		HashedPassword: "0000",
